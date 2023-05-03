a51deed9b4ba3c4025465b896e09494e4ee545a7
Todd Griffin <todd.griffin61@outlook.com>
2023-05-03 17:33:38 +0100

    finished ext 1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import {initialWorkouts, generateWorkout} from "./Workouts.js"
 import "./App.css"
 
 function App() {
+
+  const initialHideUnfinished = false
+  const [hideUnfinished, setHideUnfinished] = useState(initialHideUnfinished)
+
   const [workouts, setWorkouts] = useState(initialWorkouts)
 
   const addNewWorkout = () => {
@@ -30,25 +34,31 @@ function App() {
     console.log('workout.done', workout.done)
     const completed = workouts.map(el => {
       if (el === workout) {
-        return {
-          ...el,
-          exercise: 'completed it mate',
-          done: true
-        }
-      } else {
-        return el
-      }
+        el.done = true
+      } 
+      return el
     })
     setWorkouts(completed)
   }
 
-  return (
+  const toggleDone = (event) => {
+    const newState = event.target.checked
+    setHideUnfinished(newState)
+  }
+
+    return (
     <div className="App">
       <h1>🏋️‍♀️Workout Generator</h1>
       <button onClick={addNewWorkout}>Add New Workout</button>
+      <form onChange={toggleDone}>
+        <label>Only show finished workouts</label>
+        <input type='checkbox'></input>
+      </form>
       <ul>
+
         {workouts.map((workout, index) => (
-          <li key={index}>
+
+          <li key={index} style={hideUnfinished ? (workout.done ? {} : {display:'none'}) : {}}>
             <p>
               {workout.sets}x sets of <strong>{workout.reps}x{workout.exercise}</strong> with {workout.rest} seconds rest
             </p>
