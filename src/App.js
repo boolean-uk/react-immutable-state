fadbc1cbdd52a6cfd9c522c4181c536e6f364fd3
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-05-03 19:13:08 +0200

    Some progress with 1 extension done

--- src/App.js
+++ src/App.js
@@ -2,26 +2,85 @@ import { useState } from "react"
 import {initialWorkouts, generateWorkout} from "./Workouts.js"
 import "./App.css"
 
+
+let stateHistory = []
 function App() {
   const [workouts, setWorkouts] = useState(initialWorkouts)
 
   const addNewWorkout = () => {
     const newWorkout = generateWorkout()
-    console.log("addNewWorkout:", newWorkout)
+    // console.log("addNewWorkout:", newWorkout)
+    setWorkouts([...workouts, newWorkout])
+    
   }
 
   const deleteWorkout = (workout) => {
-    console.log("deleteWorkout:", workout)
+    // console.log("deleteWorkout:", workout)
+    const newWorkouts = workouts.filter(item => {
+      if (item !== workout) {
+        return workout
+      }
+    })
+    setWorkouts(newWorkouts)
+    
+
   }
 
   const completeWorkout = (workout) => {
-    console.log("completeWorkout:", workout)
+    
+    const newWorkouts = workouts.map(item => {
+      if (item === workout) {
+        return {
+          ...item,
+          done: true
+        }
+      } else {
+        return item
+      }
+    })
+
+    setWorkouts(newWorkouts)
+    
   }
+
+  
+
+
+  const changeListener = (event) => {
+
+    const restoreState = () => workouts.map(object => object)
+    const doneWorkouts = workouts.filter(object => object.done === true)
+    
+    if (event.target.checked === true){
+      setWorkouts(doneWorkouts)
+      
+    }
+    stateHistory.push(restoreState())
+   
+    if (event.target.checked === false) {
+      setWorkouts(stateHistory[(stateHistory.length-1) - 1])
+    } 
+    
+  }
+  
+
 
   return (
     <div className="App">
       <h1>🏋️‍♀️Workout Generator</h1>
       <button onClick={addNewWorkout}>Add New Workout</button>
+
+      {/* Add a checkbox with the label "Show Done Only" to allow the user to toggle between all workouts and only workouts that are done. */}
+
+      <div>
+        <input type = "checkbox" id = "showDone" onChange = {changeListener} />
+        <label htmlFor = "showDone">
+          Show Done Only
+        </label>
+      </div>
+
+
+
       <ul>
         {workouts.map((workout, index) => (
           <li key={index}>
@@ -29,16 +88,22 @@ function App() {
               {workout.sets}x sets of <strong>{workout.reps}x{workout.exercise}</strong> with {workout.rest} seconds rest
             </p>
             {!workout.done && 
-              <button onClick={e=>completeWorkout(workout)}>Done</button>}
+              <button onClick={()=>completeWorkout(workout)}>Done</button>}
             {workout.done && 
              <p>✅</p>}
-            <button onClick={e=>deleteWorkout(workout)}>Delete</button>
+            <button onClick={()=>deleteWorkout(workout)}>Delete</button>
+
+            {/* Add a new button to each workout that when clicked replaces the workout with another random workout */}
+
+
+
           </li>
         ))}
       </ul>
       
     </div>
   )
+
 }
 
 export default App
--- src/Workouts.js
+++ src/Workouts.js
@@ -41,3 +41,4 @@ const getRandomItem = (items) => {
 const getRandomInRange = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
