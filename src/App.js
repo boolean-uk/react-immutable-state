c8e6e542e73d9590db43b49c7f2b045973996d02
Isabelle <isabel.tartarelli@live.it>
2023-05-03 16:40:33 +0100

    Fixed component problem

--- src/App.js
+++ src/App.js
@@ -37,26 +37,6 @@ function App() {
         completeWorkout={completeWorkout}
         deleteWorkout={deleteWorkout}
       />
-      {/* <h1>🏋️‍♀️Workout Generator</h1>
-      <button onClick={addNewWorkout}>Add New Workout</button>
-      <ul>
-        {workouts.map((workout, index) => (
-          <li key={index}>
-            <p>
-              {workout.sets}x sets of{" "}
-              <strong>
-                {workout.reps}x{workout.exercise}
-              </strong>{" "}
-              with {workout.rest} seconds rest
-            </p>
-            {!workout.done && (
-              <button onClick={(e) => completeWorkout(workout)}>Done</button>
-            )}
-            {workout.done && <p>✅</p>}
-            <button onClick={(e) => deleteWorkout(workout)}>Delete</button>
-          </li>
-        ))}
-      </ul> */}
     </div>
   );
 }
--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,34 +1,34 @@
 import { useState } from "react";
 
-const completed = {
-  textDecoration: "line-through",
-};
-
 function Workout(props) {
-  <>
-    <h1>🏋️‍♀️Workout Generator</h1>
-    <button onClick={props.addNewWorkout}>Add New Workout</button>
-    <ul>
-      {props.workouts.map((workout, index) => (
-        <li key={index}>
-          <p>
-            {workout.sets}x sets of{" "}
-            <strong>
-              {workout.reps}x{workout.exercise}
-            </strong>{" "}
-            with {workout.rest} seconds rest
-          </p>
-          {!workout.done && (
-            <button onClick={(e) => props.completeWorkout(workout)}>
-              Done
+  return (
+    <>
+      <h1>🏋️‍♀️Workout Generator</h1>
+      <button onClick={props.addNewWorkout}>Add New Workout</button>
+      <ul>
+        {props.workouts.map((workout, index) => (
+          <li key={index}>
+            <p>
+              {workout.sets}x sets of{" "}
+              <strong>
+                {workout.reps}x{workout.exercise}
+              </strong>{" "}
+              with {workout.rest} seconds rest
+            </p>
+            {!workout.done && (
+              <button onClick={(e) => props.completeWorkout(workout)}>
+                Done
+              </button>
+            )}
+            {workout.done && <p>✅</p>}
+            <button onClick={(e) => props.deleteWorkout(workout)}>
+              Delete
             </button>
-          )}
-          {workout.done && <p>✅</p>}
-          <button onClick={(e) => props.deleteWorkout(workout)}>Delete</button>
-        </li>
-      ))}
-    </ul>
-  </>;
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default Workout;
