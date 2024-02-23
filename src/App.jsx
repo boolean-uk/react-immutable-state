d5b33a890d0dea2f094d8a782c2109a8077878b0
Victor <victor.adamson@se.experis.com>
2024-02-23 10:27:02 +0100

    Core done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import { useState } from "react"
-import {initialWorkouts, generateWorkout} from "./Workouts.js"
-import "./App.css"
+import { useState } from "react";
+import { initialWorkouts, generateWorkout } from "./Workouts.js";
+import "./App.css";
 
 function App() {
-  const [workouts, setWorkouts] = useState(initialWorkouts)
+  const [workouts, setWorkouts] = useState(initialWorkouts);
 
   const addNewWorkout = () => {
-    const newWorkout = generateWorkout()
-    console.log("addNewWorkout:", newWorkout)
-  }
+    const newWorkout = generateWorkout();
+    console.log("addNewWorkout:", newWorkout);
+    const addedWorkout = [...workouts, newWorkout];
+    setWorkouts(addedWorkout);
+  };
 
   const deleteWorkout = (workout) => {
-    console.log("deleteWorkout:", workout)
-  }
+    console.log("deleteWorkout:", workout);
+    const updatedWorkouts = workouts.filter(function (workouts) {
+      return workouts !== workout;
+    });
+    setWorkouts(updatedWorkouts);
+  };
 
   const completeWorkout = (workout) => {
-    console.log("completeWorkout:", workout)
-  }
+    console.log("completeWorkout:", workout);
+    const updatedWorkouts = workouts.map(function (workouts) {
+      if (workouts === workout) {
+        return { ...workouts, done: true };
+      }
+      return workouts;
+    });
+    setWorkouts(updatedWorkouts);
+  };
 
   return (
     <div className="App">
@@ -26,19 +39,22 @@ function App() {
         {workouts.map((workout, index) => (
           <li key={index}>
             <p>
-              {workout.sets}x sets of <strong>{workout.reps}x{workout.exercise}</strong> with {workout.rest} seconds rest
+              {workout.sets}x sets of{" "}
+              <strong>
+                {workout.reps}x{workout.exercise}
+              </strong>{" "}
+              with {workout.rest} seconds rest
             </p>
-            {!workout.done &&
-              <button onClick={e=>completeWorkout(workout)}>Done</button>}
-            {workout.done &&
-              <p>✅</p>}
-            <button onClick={e=>deleteWorkout(workout)}>Delete</button>
+            {!workout.done && (
+              <button onClick={(e) => completeWorkout(workout)}>Done</button>
+            )}
+            {workout.done && <p>✅</p>}
+            <button onClick={(e) => deleteWorkout(workout)}>Delete</button>
           </li>
         ))}
       </ul>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
