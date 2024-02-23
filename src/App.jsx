b2fd67cfac749fed963b901428cc2d96463243c8
Linus Willmont <linus.willmont@se.experis.com>
2024-02-23 10:05:55 +0100

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,58 @@
-import { useState } from "react"
-import {initialWorkouts, generateWorkout} from "./Workouts.js"
-import "./App.css"
+import { useState } from "react";
+import { initialWorkouts, generateWorkout } from "./Workouts.js";
+import "./App.css";
 
 function App() {
-  const [workouts, setWorkouts] = useState(initialWorkouts)
+	const [workouts, setWorkouts] = useState(initialWorkouts);
 
-  const addNewWorkout = () => {
-    const newWorkout = generateWorkout()
-    console.log("addNewWorkout:", newWorkout)
-  }
+	const addNewWorkout = () => {
+		const newWorkout = generateWorkout();
+		setWorkouts([...workouts, newWorkout]);
+		// console.log("addNewWorkout:", newWorkout);
+	};
 
-  const deleteWorkout = (workout) => {
-    console.log("deleteWorkout:", workout)
-  }
+	const deleteWorkout = (workout) => {
+		// console.log("deleteWorkout:", workout);
+		setWorkouts(workouts.filter((w) => w !== workout));
+	};
 
-  const completeWorkout = (workout) => {
-    console.log("completeWorkout:", workout)
-  }
+	const completeWorkout = (workout) => {
+		const updatedWorkouts = workouts.map((w) => {
+			if (w === workout) {
+				console.log("Found workout:", workout);
+				return { ...w, done: true };
+			} else {
+				return w;
+			}
+		});
 
-  return (
-    <div className="App">
-      <h1>🏋️‍♀️Workout Generator</h1>
-      <button onClick={addNewWorkout}>Add New Workout</button>
-      <ul>
-        {workouts.map((workout, index) => (
-          <li key={index}>
-            <p>
-              {workout.sets}x sets of <strong>{workout.reps}x{workout.exercise}</strong> with {workout.rest} seconds rest
-            </p>
-            {!workout.done &&
-              <button onClick={e=>completeWorkout(workout)}>Done</button>}
-            {workout.done &&
-              <p>✅</p>}
-            <button onClick={e=>deleteWorkout(workout)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+		setWorkouts([...updatedWorkouts]);
+	};
 
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1>🏋️‍♀️Workout Generator</h1>
+			<button onClick={addNewWorkout}>Add New Workout</button>
+			<ul>
+				{workouts.map((workout, index) => (
+					<li key={index}>
+						<p>
+							{workout.sets}x sets of{" "}
+							<strong>
+								{workout.reps}x{workout.exercise}
+							</strong>{" "}
+							with {workout.rest} seconds rest
+						</p>
+						{!workout.done && (
+							<button onClick={() => completeWorkout(workout)}>Done</button>
+						)}
+						{workout.done && <p>✅</p>}
+						<button onClick={() => deleteWorkout(workout)}>Delete</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
-export default App
+export default App;
