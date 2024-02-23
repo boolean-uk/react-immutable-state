384f51bccdcd7c2ce546f54278fd817aa87b7584
Victor <victor.adamson@se.experis.com>
2024-02-23 14:21:31 +0100

    Completed extensions 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import "./App.css";
 
 function App() {
   const [workouts, setWorkouts] = useState(initialWorkouts);
+  // add state for doneWorkouts
+  const [showDoneWorkouts, setShowDoneWorkouts] = useState(false);
 
   const addNewWorkout = () => {
     const newWorkout = generateWorkout();
     console.log("addNewWorkout:", newWorkout);
-    const addedWorkout = [...workouts, newWorkout];
-    setWorkouts(addedWorkout);
+    const updatedWorkoutsArray = [...workouts, newWorkout];
+    setWorkouts(updatedWorkoutsArray);
   };
 
   const deleteWorkout = (workout) => {
@@ -32,49 +34,90 @@ function App() {
   };
 
   const randomiseWorkout = (workout) => {
-    console.log("oldWorkout:", workout);
+    // Description: randomiseWorkout will update the current/it's own workout with data from generateWorkout()
+    // Input: workout
+    // Create new Workout object
     const updatedWorkout = generateWorkout();
-    const updatedWorkouts = workouts.map(function (workouts) {
-      if (workouts == workout) {
+    // Loop through workouts state to find current workout
+    const updatedWorkouts = workouts.map(function (existingWorkout) {
+      if (existingWorkout === workout) {
         console.log("randomiseWorkout:", updatedWorkout);
+        // If/When found, add updated workout to updatedWorkouts
         return {
-          ...workouts,
+          ...existingWorkout,
           exercise: updatedWorkout.exercise,
           reps: updatedWorkout.reps,
           sets: updatedWorkout.sets,
           rest: updatedWorkout.rest,
         };
       }
-      return workouts;
+      // Else reset it to old workout
+      return existingWorkout;
     });
+    // Change state to updatedWorkouts
     setWorkouts(updatedWorkouts);
   };
+
+  // Create an array of all "done" workouts
+  const completeWorkouts = workouts.filter((workout) => workout.done);
 
   return (
     <div className="App">
       <h1>🏋️‍♀️Workout Generator</h1>
       <button onClick={addNewWorkout}>Add New Workout</button>
-      <input type="checkbox" id="showDone"></input>
+      <input
+        type="checkbox"
+        id="showDone"
+        checked={workouts.done}
+        onChange={(event) => setShowDoneWorkouts(event.target.checked)}
+      ></input>
       <label htmlFor="showDone"> Show only done</label>
-      <ul>
-        {workouts.map((workout, index) => (
-          <li key={index}>
-            <p>
-              {workout.sets}x sets of{" "}
-              <strong>
-                {workout.reps}x{workout.exercise}
-              </strong>{" "}
-              with {workout.rest} seconds rest
-            </p>
-            {!workout.done && (
-              <button onClick={() => completeWorkout(workout)}>Done</button>
-            )}
-            {workout.done && <p>✅</p>}
-            <button onClick={() => deleteWorkout(workout)}>Delete</button>
-            <button onClick={() => randomiseWorkout(workout)}>Randomise</button>
-          </li>
-        ))}
-      </ul>
+      {/* Do a conditional render, if checkmark is not checked render all, if not render all "done" (Created earlier) */}
+      {!showDoneWorkouts ? (
+        <ul>
+          {workouts.map((workout, index) => (
+            <li key={index}>
+              <p>
+                {workout.sets}x sets of{" "}
+                <strong>
+                  {workout.reps}x{workout.exercise}
+                </strong>{" "}
+                with {workout.rest} seconds rest
+              </p>
+              {!workout.done && (
+                <button onClick={() => completeWorkout(workout)}>Done</button>
+              )}
+              {workout.done && <p>✅</p>}
+              <button onClick={() => deleteWorkout(workout)}>Delete</button>
+              <button onClick={() => randomiseWorkout(workout)}>
+                Randomise
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <ul>
+          {completeWorkouts.map((workout, index) => (
+            <li key={index}>
+              <p>
+                {workout.sets}x sets of{" "}
+                <strong>
+                  {workout.reps}x{workout.exercise}
+                </strong>{" "}
+                with {workout.rest} seconds rest
+              </p>
+              {!workout.done && (
+                <button onClick={() => completeWorkout(workout)}>Done</button>
+              )}
+              {workout.done && <p>✅</p>}
+              <button onClick={() => deleteWorkout(workout)}>Delete</button>
+              <button onClick={() => randomiseWorkout(workout)}>
+                Randomise
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
