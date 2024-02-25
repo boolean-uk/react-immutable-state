2e644cdc8c81f3abfc1e1541efffe2c369e34673
Jacob Chen Jensen <jacob.chen.jensen@dk.experis.com>
2024-02-25 16:25:18 +0100

    core

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
-import { useState } from "react"
-import {initialWorkouts, generateWorkout} from "./Workouts.js"
-import "./App.css"
+import { useState } from "react";
+import { initialWorkouts, generateWorkout } from "./Workouts.js";
+import "./App.css";
 
 function App() {
-  const [workouts, setWorkouts] = useState(initialWorkouts)
+  const [workouts, setWorkouts] = useState(initialWorkouts);
 
   const addNewWorkout = () => {
-    const newWorkout = generateWorkout()
-    console.log("addNewWorkout:", newWorkout)
-  }
+    const newWorkout = generateWorkout();
+    setWorkouts([...workouts, newWorkout]);
+  };
 
   const deleteWorkout = (workout) => {
-    console.log("deleteWorkout:", workout)
-  }
+    const newWorkouts = workouts.filter(
+      (workoutRemove) =>
+        workoutRemove.exercise === workout.exercise &&
+        workoutRemove.reps === workout.reps &&
+        workoutRemove.rest === workout.rest &&
+        workoutRemove.sets === workout.sets
+    );
+    setWorkouts(newWorkouts);
+  };
 
   const completeWorkout = (workout) => {
-    console.log("completeWorkout:", workout)
-  }
+    console.log("completeWorkout:", workout);
+    const updatedWorkout = { ...workout, done: true };
+    const newWorkouts = workouts.map((notDoneWorkout) => {
+      if (
+        notDoneWorkout.exercise === workout.exercise &&
+        notDoneWorkout.reps === workout.reps &&
+        notDoneWorkout.rest === workout.rest &&
+        notDoneWorkout.sets === workout.sets
+      )
+        return updatedWorkout;
+      return notDoneWorkout;
+    });
+    setWorkouts(newWorkouts);
+  };
 
   return (
     <div className="App">
@@ -26,19 +45,22 @@ function App() {
         {workouts.map((workout, index) => (
           <li key={index}>
             <p>
-              {workout.sets}x sets of <strong>{workout.reps}x{workout.exercise}</strong> with {workout.rest} seconds rest
+              {workout.sets}x sets of{" "}
+              <strong>
+                {workout.reps}x{workout.exercise}
+              </strong>{" "}
+              with {workout.rest} seconds rest
             </p>
-            {!workout.done &&
-              <button onClick={e=>completeWorkout(workout)}>Done</button>}
-            {workout.done &&
-              <p>✅</p>}
-            <button onClick={e=>deleteWorkout(workout)}>Delete</button>
+            {!workout.done && (
+              <button onClick={(e) => completeWorkout(workout)}>Done</button>
+            )}
+            {workout.done && <p>✅</p>}
+            <button onClick={(e) => deleteWorkout(workout)}>Delete</button>
           </li>
         ))}
       </ul>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
