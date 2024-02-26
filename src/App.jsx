44f4dccccc1912b49db4c328e33f566740441a85
Stian Gaustad <stian.gaustad@no.experis.com>
2024-02-26 09:27:22 +0100

    Core + Extension - Resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,58 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import {initialWorkouts, generateWorkout} from "./Workouts.js"
 import "./App.css"
 
 function App() {
   const [workouts, setWorkouts] = useState(initialWorkouts)
+  const [filteredWorkouts, setFilteredWorkouts] = useState(workouts)
+  const [completeOnlyCheck, setCompleteOnlyCheck] = useState(false)
 
   const addNewWorkout = () => {
     const newWorkout = generateWorkout()
-    console.log("addNewWorkout:", newWorkout)
+    setWorkouts([...workouts, newWorkout])
   }
 
   const deleteWorkout = (workout) => {
-    console.log("deleteWorkout:", workout)
+    setWorkouts(workouts.filter((w) => w !== workout))
   }
 
   const completeWorkout = (workout) => {
-    console.log("completeWorkout:", workout)
+    setWorkouts(workouts.map((w) => 
+      w === workout ? {...w, done: !w.done} : w
+    ))
   }
+
+  const toggleCheckMark = () => {
+    setCompleteOnlyCheck(!completeOnlyCheck)
+  }
+
+  useEffect(() => {
+    if (completeOnlyCheck) {
+      setFilteredWorkouts(workouts.filter((w) => w.done === completeOnlyCheck))
+    } else {
+      setFilteredWorkouts(workouts)
+    }
+  }, [workouts, completeOnlyCheck])
 
   return (
     <div className="App">
       <h1>🏋️‍♀️Workout Generator</h1>
       <button onClick={addNewWorkout}>Add New Workout</button>
+      <label>
+        <input type="checkbox" checked={completeOnlyCheck} onChange={toggleCheckMark}></input>
+        Show Done Only
+      </label>
       <ul>
-        {workouts.map((workout, index) => (
+        {filteredWorkouts.map((workout, index) => (
           <li key={index}>
             <p>
               {workout.sets}x sets of <strong>{workout.reps}x{workout.exercise}</strong> with {workout.rest} seconds rest
             </p>
             {!workout.done &&
-              <button onClick={e=>completeWorkout(workout)}>Done</button>}
+              <button onClick={() =>completeWorkout(workout)}>Done</button>}
             {workout.done &&
               <p>✅</p>}
-            <button onClick={e=>deleteWorkout(workout)}>Delete</button>
+            <button onClick={() =>deleteWorkout(workout)}>Delete</button>
           </li>
         ))}
       </ul>
