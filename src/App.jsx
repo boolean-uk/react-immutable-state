53e2e77d3d2b8152a05bdac3f91b2eec45088120
Oneallane <oneallane2808@gmail.com>
2024-02-26 10:54:16 +0100

    Core + Extension

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,69 @@ import "./App.css"
 function App() {
   const [workouts, setWorkouts] = useState(initialWorkouts)
 
+  const [allWorkouts, setAllWorkouts] = useState(initialWorkouts);
+
+
   const addNewWorkout = () => {
     const newWorkout = generateWorkout()
     console.log("addNewWorkout:", newWorkout)
+    const newWorkouts = [...workouts]
+    newWorkouts.push(newWorkout)
+    console.log(newWorkout)
+    setWorkouts(newWorkouts)
+    setAllWorkouts(workouts)
+    
+  }
+
+  const replaceWorkout= (index) => {
+    const newWorkouts = [...workouts]
+    newWorkouts[index] = generateWorkout()
+    setWorkouts(newWorkouts)
+    setAllWorkouts(workouts)
   }
 
   const deleteWorkout = (workout) => {
     console.log("deleteWorkout:", workout)
+    setWorkouts(workouts.filter(wo => wo !== workout))
+    setAllWorkouts(workouts)
+
   }
 
   const completeWorkout = (workout) => {
     console.log("completeWorkout:", workout)
+    const updateWorkout = workouts.map(function(wo) {
+      if(wo === workout) {
+        return {...wo, done: true}
+      }
+      return wo
+    })
+    setWorkouts(updateWorkout)
+    setAllWorkouts(workouts)
+  }
+
+  const filterComplete = (e) => {
+    console.log("Showing all complete workouts")
+    const isChecked = e.target.checked
+    if (!isChecked) {
+      setWorkouts(allWorkouts);
+      setAllWorkouts(workouts)
+    } else {
+      const completedWorkouts = workouts.filter((wo) => wo.done);
+      setWorkouts(completedWorkouts);
+      setAllWorkouts(workouts)
+    }
   }
 
   return (
     <div className="App">
       <h1>🏋️‍♀️Workout Generator</h1>
+      <nav className="navbar">
       <button onClick={addNewWorkout}>Add New Workout</button>
+      <label>
+        <input type="checkbox" onChange={filterComplete} />
+        Show Done Only
+      </label>
+      </nav>
       <ul>
         {workouts.map((workout, index) => (
           <li key={index}>
@@ -33,6 +79,7 @@ function App() {
             {workout.done &&
               <p>✅</p>}
             <button onClick={e=>deleteWorkout(workout)}>Delete</button>
+            <button onClick={e=>replaceWorkout(index)}>Replace</button>
           </li>
         ))}
       </ul>
